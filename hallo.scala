306bd9b705383bb2bf7fccc622a128a938e0f082
JonathanStenroos <jonathanstenroos@hotmail.com>
2021-09-10 15:06:20 +0200

    slump i meddelandet

--- hallo.scala
+++ hallo.scala
@@ -2,6 +2,6 @@ object HelloWorld {
   def main(args: Array[String]): Unit = {
     val msg: String = "Hello, world" + "!";
     println(msg + "!!!");
-    println("HEEEJDÅ");
+    println("HEEEJDÅ" * (math.random() * 10).toInt);
   }
 }
